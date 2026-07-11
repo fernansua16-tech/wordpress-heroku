9e2d644ebd9a1e4c2802b9b4ed2451d3748b42ba
fernan <fernansua16@gmail.com>
2026-07-12 01:20:10 +0530

    Update wp-config.php for Heroku Postgres
    Removed MySQL settings for Heroku Postgres configuration.

--- wp-config.php
+++ wp-config.php
@@ -14,20 +14,11 @@
  * @package WordPress
  */
 
-// ** Heroku Postgres settings - from Heroku Environment ** //
 $db = parse_url($_ENV["DATABASE_URL"]);
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
 define('DB_NAME', trim($db["path"],"/"));
-
-/** MySQL database username */
 define('DB_USER', $db["user"]);
-
-/** MySQL database password */
 define('DB_PASSWORD', $db["pass"]);
-
-/** MySQL hostname */
 define('DB_HOST', $db["host"]);
 
 /** Database Charset to use in creating database tables. */
